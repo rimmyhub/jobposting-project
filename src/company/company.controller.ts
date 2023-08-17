354129effa4438c5e688f48899b377d527fb4ba7
rimmygit <gpfla5503@gmail.com>
2023-08-17 23:39:17 +0900

    feat: company API

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,7 +1,51 @@
-import { Controller } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CompanyService } from './company.service';
+import { CreateCompanyDto } from './dto/create-company.dto';
+import { Company } from 'src/domain/company.entity';
+import { UpdateCompanyDto } from './dto/update-company.dto';
 
 @Controller('companys')
 export class CompanyController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly companyService: CompanyService) {}
+
+  // 회사 생성 + 회사 회원가입
+  @Post()
+  public async createCompany(
+    @Body() createCompanyDto: CreateCompanyDto,
+  ): Promise<Company> {
+    const company = this.companyService.createCompany({ createCompanyDto });
+    return company;
+  }
+
+  // 회사 전체 조회
+  @Get()
+  findAll() {
+    return this.companyService.findAll();
+  }
+
+  // 회사 1개 조회
+  @Get(':id')
+  finOne(@Param('id') id: string) {
+    return this.companyService.findOne(+id); //string 으로 가져와서 숫자로 변환
+  }
+
+  // 회사 수정
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
+    return this.companyService.update(+id, updateCompanyDto);
+  }
+
+  // 회사 삭제
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.companyService.remove(+id);
+  }
 }
--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,4 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Company } from 'src/domain/company.entity';
+import { Repository } from 'typeorm';
+import { CreateCompanyDto } from './dto/create-company.dto';
+import * as bcrypt from 'bcrypt';
+import { UpdateCompanyDto } from './dto/update-company.dto';
 
 @Injectable()
-export class CompanyService {}
+export class CompanyService {
+  constructor(
+    @InjectRepository(Company)
+    private readonly companyRepository: Repository<Company>,
+  ) {}
+
+  // 회사 생성 + 회사 회원가입
+  async createCompany({
+    createCompanyDto,
+  }: ICompanyServiceCreateCompany): Promise<Company> {
+    const {
+      email,
+      companyTitle,
+      password,
+      introduction,
+      website,
+      address,
+      business,
+      employees,
+    } = createCompanyDto;
+    const company = await this.companyRepository.findOne({ where: { email } });
+    if (company) throw new UnauthorizedException('이미 등록된 이메일입니다');
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newCompany = await this.companyRepository.save({
+      email,
+      companyTitle,
+      password: hashedPassword,
+      introduction,
+      website,
+      address,
+      business,
+      employees,
+    });
+    return newCompany;
+  }
+
+  // 회사 전체 조회
+  async findAll() {
+    return await this.companyRepository.find();
+  }
+
+  // 회사 1개 조회
+  async findOne(id: number) {
+    return await this.companyRepository.findOne({ where: { id } });
+  }
+
+  // 회사 수정
+  async update(id: number, updateCompanyDto: UpdateCompanyDto) {
+    const company = await this.companyRepository.findOne({ where: { id } });
+    if (!company) {
+      throw new Error('회사를 찾을 수 없습니다.');
+    }
+    Object.assign(company, updateCompanyDto);
+    return await this.companyRepository.save(company);
+  }
+
+  async remove(id: number) {
+    const company = await this.companyRepository.findOne({ where: { id } });
+    if (!company) {
+      throw new Error('회사를 찾을 수 없습니다.');
+    }
+    return await this.companyRepository.remove(company);
+  }
+}
+
+interface ICompanyServiceCreateCompany {
+  createCompanyDto: CreateCompanyDto;
+}
