e5212f905ce49769f3cfdbe245254c4ee3e69352
taeyoungNew <dndbxhd10@naver.com>
2023-08-22 02:02:33 +0900

    [feat] 회사로그인 추가

--- views/router/router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/user-signup', (req, res) => {
-  return res.render('signupUser');
-});
-
-module.exports = router;
