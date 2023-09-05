81b5f054ae8cb01dda0379ec61f0366a1dc7d682
Yoonzero <playlist808@kakao.com>
2023-09-05 23:28:22 +0900

    feat:myPage err handling

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,40 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Client } from '@opensearch-project/opensearch';
-import { AwsSigv4Signer } from '@opensearch-project/opensearch/aws';
+import {
+  OpenSearchClient,
+  AcceptInboundConnectionCommand,
+  AcceptInboundConnectionCommandInput,
+} from '@aws-sdk/client-opensearch';
 
 const AWS = require('aws-sdk');
 const configService = new ConfigService();
 
+// const protocol = 'http://localhost/';
+// const protocol = 'https://;
+// const port = 9200;
+
 @Injectable()
 export class SearchService {
-  private readonly client: Client;
+  private readonly client: OpenSearchClient;
 
+  // 클라이언트 설정 및
   constructor() {
-    this.client = new Client({
-      ...AwsSigv4Signer({
-        region: configService.get('REGION'),
-        service: configService.get('SERVICE'),
-        getCredentials: () =>
-          new Promise((resolve, reject) => {
-            AWS.config.getCredentials((err, credentials) => {
-              if (err) {
-                console.error('Error getting AWS credentials:', err);
-                reject(err);
-              } else {
-                resolve(credentials);
-              }
-            });
-          }),
-      }),
-      node: configService.get('HOST'),
-    });
+    this.client = new OpenSearchClient({ region: 'us-east-1' });
+
+    const params: AcceptInboundConnectionCommandInput = {
+      /** input parameters */
+      ConnectionId: '123',
+    };
+    const command = new AcceptInboundConnectionCommand(params);
   }
 
+  // 인덱스 생성 함수
   async createIndex() {
-    console.log('Creating index:');
-
-    const index_name = 'winner';
+    console.log('Creating index:'); // 콘솔확인 OK
+    const index_name = 'winner'; // 생성할 인덱스 네임
     const settings = {
       settings: {
         index: {
@@ -44,28 +41,46 @@ export class SearchService {
       },
     };
 
-    var response = await this.client.indices.create({
-      index: index_name,
-      body: settings,
-    });
+    // var response = await this.client.create({
+    //   index: index_name,
+    //   body: settings,
+    // });
 
-    console.log(response.body);
-    return response;
+    // console.log(response.body);
+    // return response;
   }
 
+  // 검색 기능
   async searchJobs(text: string) {
-    console.log('hi');
-    const indexes = await this.client.search({
-      index: 'winner',
-      body: {
-        query: {
-          match: { title: text },
-        },
-      },
-    });
-    console.log('123');
-    console.log(indexes);
+    console.log('hi'); // 콘솔 확인 OK
+    // const indexes = await this.client.search({
+    //   index: 'winner',
+    //   body: {
+    //     query: {
+    //       match: { title: text },
+    //     },
+    //   },
+    // });
+    // console.log('123');
+    // console.log(indexes);
 
-    return indexes;
+    // return indexes;
   }
 }
+
+// node: configService.get('HOST'), // aws opensearch 엔드포인트
+// ...AwsSigv4Signer({
+//   region: configService.get('REGION'), // 버지니아 북부 us-east-1
+//   service: configService.get('SERVICE'), // es
+//   getCredentials: () =>
+//     new Promise((resolve, reject) => {
+//       AWS.config.getCredentials((err, credentials) => {
+//         if (err) {
+//           console.error('Error getting AWS credentials:', err);
+//           reject(err);
+//         } else {
+//           resolve(credentials);
+//         }
+//       });
+//     }),
+// }),
