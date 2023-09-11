baf7e7bf44f80de4ffa32535d339e8c0bd240978
rimmygit <gpfla5503@gmail.com>
2023-09-11 12:47:47 +0900

    feat : crawling companyId uuid로 변경

--- src/jobcrawler/jobcrawler.service.ts
+++ src/jobcrawler/jobcrawler.service.ts
@@ -1,16 +1,16 @@
-// 진짜최종
-
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Jobposting } from 'src/domain/jobposting.entity';
 import { Repository } from 'typeorm';
-// import puppeteer from 'puppeteer';
 import cheerio from 'cheerio';
 import { Job } from 'src/utils/job.interface';
 import { faker } from '@faker-js/faker';
 import { Company } from 'src/domain/company.entity';
 import * as iconv from 'iconv-lite';
 import axios from 'axios';
+import { v5 as uuidv5, NIL as NIL_UUID } from 'uuid';
+import { timeEnd } from 'console';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class JobcrawlerService {
@@ -23,10 +23,7 @@ export class JobcrawlerService {
 
   private async getAxiosData(url: string): Promise<string> {
     try {
-      const response = await axios.get(
-        url,
-        { responseType: 'arraybuffer' }, // Specify response type as arraybuffer
-      );
+      const response = await axios.get(url, { responseType: 'arraybuffer' });
 
       const decodedHtml = iconv.decode(Buffer.from(response.data), 'EUC-KR');
       return decodedHtml;
@@ -49,51 +46,36 @@ export class JobcrawlerService {
         .trim();
       const task = taskWithoutDateAndExtra || '기타';
       tasks.push(task);
-
-      //   const jobText = $(node).find('.cl_btm > span').text().trim();
-      //   const jobWithoutDate = jobText.replace(/~\d{2}.\d{2}.*?\)/, '').trim();
-      //   const jobWithoutDateAndExtra = jobWithoutDate
-      //     .replace(/\([^)]+\)$/gm, '')
-      //     .trim();
-      //   const job = jobWithoutDateAndExtra || '기타';
-      // });
     });
-    console.log('tasks=============', tasks);
+
     return tasks;
   }
 
   // 회사 정보 파싱
   companyParsing(page) {
     const $ = cheerio.load(page);
-    // cheerio를 사용하여 주어진 HTML페이지를 로드하고 $ 변수에 해당 페이지의 선택자를 사용하여 조작할 수 있는 객체를 할당.
     const $companyList = $('.conts_box');
-    // .const_box에 해당하는 Html요소들을 변수에 할당합니다.
     const companies = [];
-    // 추출한 기업 정보를 저장할 배열을 초기화 합니다.
 
     $companyList.filter((idx, node) => {
-      // companyList의 요소를 필터링하고, 각 요소에 대해 작업을 수행
-
       const companyNameLink = $(node).find('.jcinfo_logo_cpname a');
-      const companyIdHtef = companyNameLink.attr('href');
-      if (companyIdHtef !== undefined) {
-        const trimmedCompanyId = companyIdHtef.trim();
-        const companyId = trimmedCompanyId.split('/').pop();
+      const companyUrl = companyNameLink.attr('href');
+      if (companyUrl !== undefined) {
+        const trimmedCompanyId = companyUrl.trim();
+
+        const companyId = uuidv5(trimmedCompanyId, NIL_UUID);
+        // const companyId = trimmedCompanyId.split('/').pop();
 
         const email = faker.internet.email();
-        // faker를 통해 가짜 데이터를 생성
 
         const password = faker.internet.password();
-        // faker를 통해 가짜 데이터를 생성
 
         const introduction = faker.lorem.paragraph();
-        // faker를 통해 가짜 데이터를 생성
 
         const title = $(node).find('.jcinfo_logo_cpname em a').text().trim();
         if (title === '') {
           return false;
         }
-        // 기업이름을 추출하고 공백을 제거하여 할당, 이름이 없을 경우 건너 뜀
 
         const business =
           $(node).find("li:contains('업종') .txt em").text().trim() || '기타';
@@ -113,20 +95,8 @@ export class JobcrawlerService {
         const image =
           'http:' + $(node).find('div.jcinfo_logo img').attr('src') || '';
 
-        // const $jobList = $('.c_row');
-        // const taskText = $(node).find('.cl_btm > span').text().trim();
-        // const taskWithoutDate = taskText
-        //   .replace(/~\d{2}.\d{2}.*?\)/, '')
-        //   .trim();
-        // const taskWithoutDateAndExtra = taskWithoutDate
-        //   .replace(/\([^)]+\)$/gm, '')
-        //   .trim();
-        // const task = taskWithoutDateAndExtra || '기타';
-        // tasks.push(task);
-
-        // 컴퍼니를 변수에 할당
         const company = {
-          id: Number(companyId),
+          id: companyId,
           email,
           password,
           title,
@@ -137,7 +107,6 @@ export class JobcrawlerService {
           address,
           website,
         };
-        // 컴퍼니스에 푸쉬
         companies.push(company);
       }
     });
@@ -152,17 +121,17 @@ export class JobcrawlerService {
     const jobs = [];
 
     $jobList.each((idx, node) => {
-      // const companyTitle = $(node).find('em a').text().trim();
-
       const companyIdLink = $(node).find('em a');
-      const companyId = companyIdLink.attr('href').split('/').pop();
+      const companyUrl = companyIdLink.attr('href');
+
+      const companyId = uuidv5(companyUrl, NIL_UUID);
 
       const title =
         $(node)
           .find('strong')
           .text()
           .replace(/\t/g, '')
-          .replace(/D-\d+/, '') // "D-" 다음에 있는 숫자를 제거
+          .replace(/D-\d+/, '')
           .trim() || '회사 이름';
 
       const career =
@@ -181,17 +150,15 @@ export class JobcrawlerService {
 
       const content = faker.lorem.paragraph();
 
-      // const dueDate = faker.date.future();
       let dueDate;
 
       const $dueDate = $('.fleft');
       $dueDate.each((idx, node) => {
         dueDate = $(node).find('.day:eq(1) em').text().trim() || '상시 채용';
-        // dueDate = jobs[idx].dueDate;
       });
 
       const jobposting = {
-        companyId: Number(companyId),
+        companyId,
         job: task,
         title,
         career,
@@ -209,6 +176,7 @@ export class JobcrawlerService {
     return jobs;
   }
 
+  @Cron('0 8 * * *') // 매일 오전 8시에 실행
   async incruitCrawling() {
     console.time('코드 실행시간');
     // 코드 실행시간을 구해줌
@@ -217,27 +185,18 @@ export class JobcrawlerService {
     const jobInfo = [];
     const companyInfo = [];
 
-    // 페이지수 만큼 반복문을 돌며 정보를 크롤링
-    // 해당 페이지의 URL을 생성하고, 이를 getPuppeteerData통해 Puppeteer를 통해 가져오기를 사용
     for (let i = 1; i <= totalPage; i++) {
       const jobpostingUrl = `https://job.incruit.com/jobdb_list/searchjob.asp?ct=3&ty=1&cd=149&page=${i}`;
       const jobpostingContent = await this.getAxiosData(jobpostingUrl);
-
-      // Cheerio를 사용하여 페이지의 HTML을 파싱하고, jobLinks선택하여 채용공고의 링크를 제공
       const $ = cheerio.load(jobpostingContent);
 
       const tasksContent = await this.getAxiosData(jobpostingUrl);
       const tasks = this.taskParsing(tasksContent);
-      console.log(tasks);
 
       const jobLinks = $('li.c_col .cell_mid .cl_top a');
       let count = 0;
       for (const jobLink of jobLinks) {
-        // 채용공고의 링크를 순회해서 상세페이지의 HTML을 가져오고
-
         const jobDetailUrl = $(jobLink).attr('href');
-
-        console.log('================jobLinks===============', jobDetailUrl);
 
         const companyContent = await this.getAxiosData(jobDetailUrl);
 
@@ -249,22 +208,20 @@ export class JobcrawlerService {
         companyInfo.push(companies);
         jobInfo.push(jobs);
         count++;
-        // [[company, company], [company, company], [company, company], []]
 
         await this.delay(1000); // 각페이지 크롤링 후 1초 대기
       }
     }
     // 코드 실행시간을 측정
+    console.log(timeEnd);
+
     console.log('===========companyInfo===========', companyInfo);
     console.log('==========jobInfo================', jobInfo);
-
     // companyEntities에 담을 초기화 배열
 
     for (const companys of companyInfo) {
       for (const company of companys) {
-        // const companyEntities = [];
         const companyEntity = this.companyRepository.create({
-          // 생성하고
           id: company.id,
           email: company.email,
           password: company.password,
@@ -276,7 +233,6 @@ export class JobcrawlerService {
           website: company.website,
           address: company.address,
         });
-        // companyEntities.push(companyEntity);
         const isExist = await this.companyRepository.findOne({
           where: { id: company.id },
         });
@@ -285,20 +241,12 @@ export class JobcrawlerService {
         }
       }
     }
-    // console.log(
-    //   '==============companyEntities================',
-    //   companyEntities,
-    // );
-
-    // 회사 DB에 저장한다
 
     for (const jobs of jobInfo) {
       for (const job of jobs) {
         const jobEntity = this.jobpostingRepository.create({
-          // 생성하고
           companyId: job.companyId,
           company: { id: job.companyId },
-          // companyId: company.id,
           title: job.title,
           career: job.career,
           salary: job.salary,
@@ -309,11 +257,10 @@ export class JobcrawlerService {
           content: job.content,
           dueDate: job.dueDate,
         });
-        this.jobpostingRepository.insert(jobEntity); // 채용공고 DB에 저장한다
+        this.jobpostingRepository.insert(jobEntity);
       }
     }
 
-    // 리턴값 보여주기..
     return { jobInfo, companyInfo };
   }
   private delay(ms: number) {
