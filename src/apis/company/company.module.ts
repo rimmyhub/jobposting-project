53c5d236a4e7019c2c85bc2cdbd3718e0d8c092d
rimmygit <gpfla5503@gmail.com>
2023-11-02 19:04:48 +0900

    feat : company-cache

--- src/apis/company/company.module.ts
+++ src/apis/company/company.module.ts
@@ -4,9 +4,14 @@ import { CompanyService } from './company.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MailModule } from '../mail/mail.module';
 import { Company } from '../domain/company.entity';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Company]), MailModule],
+  imports: [
+    CacheModule.register(),
+    TypeOrmModule.forFeature([Company]),
+    MailModule,
+  ],
   controllers: [CompanyController],
   providers: [CompanyService],
   exports: [CompanyService, TypeOrmModule],
